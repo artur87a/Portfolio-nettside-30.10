025ee9e652c2807e8abc6896f5c724d678b44bc2
artur.kodehode@gmail.com <artur.kodehode@gmail.com>
2023-11-08 13:25:00 +0100

    v7

---
+++ mailer.js
@@ -0,0 +1,23 @@
+var btn = document.getElementById("btn")
+btn.addEventListener("click", function(e){
+    e.preventDefault()
+    console.log("hi");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    var body = "name: " +name + "<br/> email: " + email + "<br/> subject: " +subject + "<br/> message: " + message;
+
+
+                    Email.send({
+        SecureToken : "a19d3a27-0c88-4492-8eb5-56921e8ea43c",
+        From : 'artur.kodehode@gmail.com',
+        To : document.getElementById("email").value,
+        Subject : "Contact message",
+        Body : body
+    }).then(
+      message => alert(message)
+    );
+
+})
+
